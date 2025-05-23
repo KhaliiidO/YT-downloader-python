e0498342c6337e750735903cfd74f5eb6b98ff47
KhaliiidO <kzaroual.dev@gmail.com>
2025-05-23 15:07:30 +0100

    Add main script file

---
+++ YT-downloader.py
@@ -0,0 +1,13 @@
+from pytube import YouTube
+
+url = input("Enter the YouTube video URL: ")
+yt = YouTube(url)
+
+print("Title:", yt.title)
+print("Length:", yt.length, "seconds")
+print("Views:", yt.views)
+
+stream = yt.streams.get_highest_resolution()
+print("Downloading...")
+stream.download()
+print("Download complete!")
